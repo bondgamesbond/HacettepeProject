ccf782c8aa358882a0c73140fd59e168d2eee5c8
Ali Onur Geven <gevendary@gmail.com>
2016-11-08 14:45:16 +0300

    Menu Interface Added

--- Assets/Scripts/ButtonUtility.cs
+++ Assets/Scripts/ButtonUtility.cs
@@ -8,10 +8,12 @@ public class ButtonUtility : MonoBehaviour {
     public InputField username, password;
     public GameObject gameList, signInMenu, signInMenu_back;
     public GameObject errorPanel;
-    
-    
-	// Use this for initialization
-	void Start () {
+    private bool firstTime = true;
+    VirtualKeyboard vk = new VirtualKeyboard();
+
+
+    // Use this for initialization
+    void Start () {
 	    if (PlayerPrefs.GetInt("Babuş") == 1)
         {
             gameList.SetActive(true);
@@ -43,7 +45,39 @@ public class ButtonUtility : MonoBehaviour {
             errorPanel.SetActive(true);
         }
     }
-	// Update is called once per frame
+    // Update is called once per frame
+
+   
+
 	void Update () {
+        if (firstTime)
+        {
+            if (username.isFocused || password.isFocused)
+            {
+                OpenKeyboard();
+                firstTime = false;
+            }
+            else
+            {
+                CloseKeyboard();
+                firstTime = true;
+            }
+        }
+
 	}
+   
+
+    public void OpenKeyboard()
+    {
+        {
+            vk.ShowTouchKeyboard();
+        }
+    }
+
+    public void CloseKeyboard()
+    {
+        {
+            vk.HideTouchKeyboard();
+        }
+    }
 }
---
+++ Assets/Scripts/DoctorSceneManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DoctorSceneManager : MonoBehaviour {
+    public GameObject chartsMenu, patientsMenu, gamesMenu;
+    public GameObject RMPMenu;
+    public GameObject pieChartPrefab, barChartPrefab;
+    public GameObject patientName1, patientName2;
+
+    public void selectPatient()
+    {
+        patientsMenu.SetActive(false);
+        gamesMenu.SetActive(true);
+        patientName1.GetComponent<Text>().text = patientsMenu.transform.GetChild(2).transform.GetChild(0).GetComponent<Text>().text;
+        patientName2.GetComponent<Text>().text = patientsMenu.transform.GetChild(2).transform.GetChild(0).GetComponent<Text>().text;
+
+    }
+    public void selectGame()
+    {
+        gamesMenu.SetActive(false);
+        chartsMenu.SetActive(true);
+    }
+    public void selectRMP()
+    {
+        gamesMenu.SetActive(false);
+        RMPMenu.SetActive(true);
+    }
+    public void backToPatientsMenu()
+    {
+        gamesMenu.SetActive(false);
+        patientsMenu.SetActive(true);
+    }
+    public void backToGamesMenu()
+    {
+        RMPMenu.SetActive(false);
+        chartsMenu.SetActive(false);
+        gamesMenu.SetActive(true);
+    }
+    public void changeSceneToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void openPieChart()
+    {
+        pieChartPrefab.SetActive(true);
+        barChartPrefab.SetActive(false);
+    }
+    public void openBarChart()
+    {
+        pieChartPrefab.SetActive(false);
+        barChartPrefab.SetActive(true);
+    }
+}
